110f31940c67d1de53f6f0f6e9365f4c56a8cb1a
MR-Mehmood <160325537+MR-Mehmood@users.noreply.github.com>
2024-08-29 03:58:17 +0500

    this is chapter 2 index2.js page and code

---
+++ index2.js
@@ -0,0 +1,33 @@
+// section-numbar-2
+
+// Q-num-1
+var username;
+
+// Q-num-2
+var myName = "MEHMOOD JAMIL"; 
+alert("Full Name:" + myName);
+
+// Q-num-3
+var message;
+message = "Hello World";
+alert(message);
+
+// Q-num-4
+var studentName = "MEHMOOD JAMIL"; 
+var studentAge = 18; 
+var studentGrade = "A"; 
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Grade: " + studentGrade);
+
+// Q-num-5
+var alertMessage = "pizza \n pizz \n piz \n pi \n p";
+alert(alertMessage);
+
+// Q-num-6
+var email = "balochsami479@gmail.com"; 
+alert("My email address is: " + email);
+
+// Q-num-7
+var book = "A English way to learn english language";
+alert("My favorite book is: " + book);
