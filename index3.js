dafe7d31b2ecc21b131c9996319633f8acd0feb2
MR-Mehmood <160325537+MR-Mehmood@users.noreply.github.com>
2024-08-29 04:02:22 +0500

    this is chapter 3 index3.js page and code

---
+++ index3.js
@@ -0,0 +1,24 @@
+// section-numbar-3
+
+
+
+// Q-num-1
+var age = 15; 
+alert("I am " +age+ " years old");
+
+// Q-num-2
+var visits = 15;
+alert("You have visited this site " + visits + " times.");
+
+// Q-num-3
+ var birthYear = 2005; 
+ document.write("My birth year is " + birthYear + ".<br>");
+ document.write("I am " + (2024 - birthYear) + " years old. <br>");
+
+
+// Q-num-4
+ var visitorName = "Kifal Hamad";
+ var productTitle = "T-shirt";
+ var quantity = 10;
+
+ document.write(visitorName + " ordered " + quantity + " " + productTitle + "(M) on alxzanda Clothing store.");
