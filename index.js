4f9e3c3624dfc4e434530bf12154055733fa7d1e
MR-Mehmood <160325537+MR-Mehmood@users.noreply.github.com>
2024-08-29 03:54:52 +0500

    this is  index.js page and code

---
+++ index.js
@@ -0,0 +1,21 @@
+// section-numbar-1
+
+// Q-num-1 
+var myname = "Wellcome To My Ecommerce Website";
+alert(myname);
+
+// Q-num-2
+var myname = "Welcome To My\nEcommerce Website";
+alert(myname);
+
+// Q-num-3
+var myname = "Welcome To javascript";
+alert(myname);
+
+// Q-num-4
+var myname = "Happy Coding\nWellcome To javascript Website";
+alert(myname);
+
+// Q-num-5
+var myname = "Hi. This Is MEHMOOD I'M Student Of Frendnt Divalpar ";
+alert(myname);
